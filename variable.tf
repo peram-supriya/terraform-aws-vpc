6e760ae7ae333bff9ceac35f032ca70fd2344359
peram-supriya <perampriya26@gmail.com>
2026-09-15 19:47:12 +0530

    actions

--- variable.tf
+++ variable.tf
@@ -51,7 +51,7 @@ variable "private_subnet_tags" {
 
 variable "database_subnet_cidrs" {
     type = list(string)
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
   
 }
 
