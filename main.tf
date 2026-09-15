d378061beedf5206bc90586a893e77b5a71eb381
peram-supriya <perampriya26@gmail.com>
2026-09-15 19:43:27 +0530

    actions

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw_final_tags
 }
 
-
-resource "aws_subnet" "main" {
+#public subnet
+resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
@@ -26,6 +26,39 @@ resource "aws_subnet" "main" {
     local.comman_tags, {
       #roboshop-dev.public.us-east-1a
       Name = "${var.project}-${var.environment}.public-${local.az_zones[count.index]}"
+    }
+  )
+}
+
+#private subnet
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = local.az_zones[count.index]
+  map_public_ip_on_launch = false
+
+  tags = merge(
+    local.comman_tags, {
+      #roboshop-dev.private.us-east-1a
+      Name = "${var.project}-${var.environment}.private-${local.az_zones[count.index]}"
+    }
+  )
+}
+
+# database subnet
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_zones[count.index]
+  map_public_ip_on_launch = false
+
+  tags = merge(
+    local.comman_tags, {
+      #roboshop-dev.databse.us-east-1a
+      Name = "${var.project}-${var.environment}.database-${local.az_zones[count.index]}"
     }
   )
 }
--- variable.tf
+++ variable.tf
@@ -37,3 +37,25 @@ variable "public_subnet_tags" {
     type = map
     default = {}
 }
+
+variable "private_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+  
+}
+
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list(string)
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
+  
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
