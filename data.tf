4ed4aa45a247e3c6d89d6316f12469ac5867e3b6
peram-supriya <perampriya26@gmail.com>
2026-09-22 12:46:40 +0530

    actions

--- data.tf
+++ data.tf
@@ -5,3 +5,15 @@ data "aws_availability_zones" "available" {
 data "aws_vpc" "default_vpc" {
   default = true
 }
+
+
+
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
