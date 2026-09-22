f4a6aa7d93f8b01226c46fad5288946b6bfe59d8
peram-supriya <perampriya26@gmail.com>
2026-09-22 14:03:23 +0530

    actions

--- peering.tf
+++ peering.tf
@@ -5,6 +5,8 @@ resource "aws_vpc_peering_connection" "default" {
     peer_vpc_id   = data.aws_vpc.default_vpc.id
     vpc_id        = aws_vpc.main.id
 
+    auto_accept = true
+
     accepter {
         allow_remote_vpc_dns_resolution = true
     }
@@ -23,13 +25,28 @@ resource "aws_vpc_peering_connection" "default" {
 resource "aws_route" "public_peering" {
     count = var.is_peering_required ? 1 : 0 
     route_table_id            = aws_route_table.public.id
-    destination_cidr_block    = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
+    destination_cidr_block    = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
+
+resource "aws_route" "private_peering" {
+    count = var.is_peering_required ? 1 : 0 
+    route_table_id            = aws_route_table.private.id
+    destination_cidr_block    = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+    count = var.is_peering_required ? 1 : 0 
+    route_table_id            = aws_route_table.database.id
+    destination_cidr_block    = data.aws_vpc.default_vpc.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
 
 resource "aws_route" "default_peering" {
     count = var.is_peering_required ? 1 : 0 
     route_table_id            = data.aws_route_table.default.id
     destination_cidr_block    = var.vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
